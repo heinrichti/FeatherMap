e2f7b30f59435dcbc001cc0580a92ef32721cad4
Tia Heinrich <2929650+heinrichti@users.noreply.github.com>
2020-06-09 20:18:46 +0200

    Refactorings

---
+++ FeatherMap/PropertyAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace FeatherMap
+{
+    internal static class PropertyAccess
+    {
+        internal static Action<T, TProp> CreateSetter<T, TProp>(PropertyInfo propertyInfo)
+        {
+            // TODO this is significantly slower on .NET Framework than propertyInfo.GetSetMethod().CreateDelegate(typeof(Action<T, TProp>));
+            var instance = Expression.Parameter(typeof(T));
+            var argument = Expression.Parameter(typeof(TProp));
+
+            var propertySetMethod = propertyInfo.GetSetMethod();
+
+            var setterCall = Expression.Call(
+                instance,
+                propertySetMethod,
+                argument);
+
+            return (Action<T, TProp>)Expression.Lambda(setterCall, instance, argument).Compile();
+        }
+
+        internal static Func<T, TProp> CreateGetter<T, TProp>(PropertyInfo propertyInfo)
+        {
+            // TODO this is significantly slower on .NET Framework than propertyInfo.GetGetMethod().CreateDelegate(typeof(Func<T, TProp>));
+            var instance = Expression.Parameter(typeof(T));
+            var property = Expression.Property(instance, propertyInfo);
+            return (Func<T, TProp>)Expression.Lambda(property, instance).Compile();
+        }
+    }
+}
